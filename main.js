68eb7e22efe2f5cc06bb8483cbadaeeb0730b432
IAMDEH <ee_izri@esi.dz>
2020-03-07 12:01:59 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 8080;
+
+console.log(process.env.ENVIRONMENT || 'e2e');
+
+console.log(process.env.ENVIRONMENT || 'prod');
+
+/*if (process.env.ENVIRONMENT === 'prod') {	
+    process.exit(1);	
+}*/
+
+const server = http.createServer((_, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, Jenkins World!\n');
+});
+
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
